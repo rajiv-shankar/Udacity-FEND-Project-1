e47dbd2abab0262afb7ef327d0cca2d97d32292d
Rajiv Shankar <rajiv.shankar.email@gmail.com>
2018-09-26 19:38:15 -0400

    Revise main.css & main.js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// to open drawer when navicon clicked (navicon only appears in NARROW screen)
+
+var menu = document.querySelector('#menu');
+var main = document.querySelector('main');
+var drawer = document.querySelector('#drawer');
+
+menu.addEventListener('click', function(e) {
+  drawer.classList.toggle('open');  // 'classList' returns element class name(s)
+  e.stopPropagation();
+});
+main.addEventListener('click', function() {
+  drawer.classList.remove('open');
+});
